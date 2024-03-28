1f68a12db3546f3c5ec892796eaafe21355efe42
Kaan Kırlı <73934843+kaankirlii@users.noreply.github.com>
2024-03-28 16:31:42 +0300

    Add files via upload

---
+++ SimpleWebsiteMVC/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleWebsiteMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "This page is About page.";
+
+            return View();
+        }
+        public ActionResult Skills()
+        {
+            ViewBag.Message = "This page is Skills page.";
+
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact with me";
+
+            return View();
+        }
+    }
+}
